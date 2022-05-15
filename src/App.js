a16cdfc872a17ca46a2710f559ed21913bd38aa1
Gustavo 친수 Giuliano <61397269+JinsuGiuliano@users.noreply.github.com>
2022-05-15 02:54:39 +0200

    Enhaced Signin UI and UseEffect fail on fetcFollowers

--- src/App.js
+++ src/App.js
@@ -2,22 +2,18 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { checkUserSession, fetchFollowStart, fetchUsersStart } from './redux/user/user.actions';
 import Home from './pages/home/home.component';
-import { GlobalStyle } from './globalStyles';
 import ErrorBoundary from './ErrorBoundary'
 import { postFetchStart } from './redux/posts/posts.actions';
-import { selectCurrentUser } from './redux/user/user.selectors';
-import { messageFetchStart } from './redux/messages/messages.actions';
 // import { addCollectionAndDocuments } from './firebase/firebase.utils';
 // import SHOP_DATA from './redux/shop/shop.data';
 const App = () => {
 
     const dispatch = useDispatch();
-  
     useEffect(()=>{
         dispatch(checkUserSession());
         dispatch(postFetchStart());
         dispatch(fetchUsersStart());
-        dispatch(fetchFollowStart());
+        // dispatch(fetchFollowStart());
     },[])
 
     return(
