85df3b91eeac8b2e08c0235dfbd0ebed3f654914
Gustavo 친수 Giuliano <61397269+JinsuGiuliano@users.noreply.github.com>
2022-05-17 12:00:19 +0200

    profile ui & button follow

--- src/pages/profile/profilePage.component.jsx
+++ src/pages/profile/profilePage.component.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Outlet, Route, Routes } from 'react-router-dom';
 
 import Profile from './profile/profile.component';
 import ProfileById from './profile/profileById.component';
