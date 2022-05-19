5a39fa9300a04e57427a32756a2b5b9adcfc4ffb
Gustavo 친수 Giuliano <61397269+JinsuGiuliano@users.noreply.github.com>
2022-05-19 11:26:18 +0200

    Hola

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { checkUserSession, fetchUsersStart } from './redux/user/user.actions';
+import Home from './pages/home/home.component';
+import ErrorBoundary from './ErrorBoundary'
+import { postFetchStart } from './redux/posts/posts.actions';
+
+const App = () => {
+
+    const dispatch = useDispatch();
+    useEffect(()=>{
+        dispatch(checkUserSession());
+        dispatch(postFetchStart());
+        dispatch(fetchUsersStart());
+    },[])
+
+    return(
+      <ErrorBoundary>
+            <Home/>
+      </ErrorBoundary>
+
+    );
+  }
+
+
+export default App;
